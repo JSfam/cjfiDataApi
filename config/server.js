d20cf99636a24eeb130ee02c7442b9946ff544a7
Jaime Ofamen <51147496+JSfam@users.noreply.github.com>
2020-10-12 01:39:05 +0800

    First Push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c77cd704ed189edb7922e9e42a33599c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd1916778-c0f8-4176-96c0-9cf2cf744bfc'
+};
